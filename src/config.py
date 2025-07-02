2d6d1e2be2b690841f4cff7e14a839b9810d8767
Kacper Rabczewski <krabczewski@gmail.com>
2025-07-03 01:35:04 +0200

    Add data preparation step.

--- src/config.py
+++ src/config.py
@@ -4,8 +4,10 @@ PROJECT_ROOT = Path(__file__).parent.parent
 
 DATA_DIR = PROJECT_ROOT / "data"
 RAW_DATA_DIR = DATA_DIR / "raw"
+PROCESSED_DATA_DIR = DATA_DIR / "processed"
 
 LOG_DIR = PROJECT_ROOT / "logs"
 
 RAW_DATA_DIR.mkdir(parents=True, exist_ok=True)
+PROCESSED_DATA_DIR.mkdir(parents=True, exist_ok=True)
 LOG_DIR.mkdir(parents=True, exist_ok=True)
