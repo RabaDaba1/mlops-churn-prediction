734defe538d0d4b9e77d7f3585260e962613f176
Kacper Rabczewski <krabczewski@gmail.com>
2025-07-03 01:21:27 +0200

    Improve error handling in the download_dataset.py.

--- src/data_collection.py
+++ src/data_collection.py
@@ -13,25 +13,22 @@ logger = get_logger("data_collection")
 def download_dataset(output_path: Path):
     dataset_name = "muhammadshahidazeem/customer-churn-dataset"
     file_path = "customer_churn_dataset-testing-master.csv"
+
     logger.info(f"Downloading dataset: {dataset_name}")
 
     try:
         df: pd.DataFrame = kagglehub.dataset_load(
             KaggleDatasetAdapter.PANDAS, dataset_name, file_path
         )
-
-        logger.info(f"Dataset downloaded successfully. Saving to {output_path}")
-
-        df.to_csv(output_path / file_path, index=False)
-
-        logger.info("Dataset download and copy complete.")
     except Exception as e:
-        logger.error(f"Failed to download or copy dataset. Error: {e}")
-        logger.error(
-            "Please ensure your Kaggle API credentials are set up correctly. "
-            "Place your kaggle.json file in ~/.kaggle/kaggle.json"
-        )
+        logger.error(f"Failed to download dataset. Error: {e}")
         raise
+
+    logger.info(f"Dataset downloaded successfully. Saving to {output_path}")
+
+    df.to_csv(output_path / file_path, index=False)
+
+    logger.info("Dataset download and copy complete.")
 
 
 if __name__ == "__main__":
